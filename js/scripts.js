169feec3fe774d95e6371d12ff50c6187d7ccf54
D4RKAR117 <D4RKAR117@outlook.es>
2019-01-20 02:23:40 -0500

    Initial Comit

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+const SRSettings = {
+	reset: true,
+	useDelay: 'onload',
+	// * Disabeled for horizontal scroll on mobile clients
+	mobile: false,
+};
+
+const SrClassOptions = {
+	duration: 4600,
+	scale: 0.85,
+	easing: 'ease-in-out',
+}
+
+const RightOptions = {
+	duration: 3400,
+	origin: 'right',
+	easing: 'ease-in-out',
+	distance: '10%'
+};
+
+const LeftOptions = {
+	duration: 3400,
+	origin: 'left',
+	easing: 'ease-in-out',
+	distance: '10%'
+};
+
+const TopOptions = {
+	duration: 3400,
+	origin: 'top',
+	easing: 'ease-in-out',
+	distance: '10%'
+};
+
+const BottomOptions = {
+	duration: 3400,
+	origin: 'bottom',
+	easing: 'ease-in-out',
+	distance: '10%'
+};
+
+const LogoOptions = {
+	duration: 2700,
+	origin: 'top',
+	distance: '10%'
+};
+
+document.addEventListener('DOMContentLoaded', function (event) {
+	ScrollReveal(SRSettings);
+	ScrollReveal().reveal('.reveal-logo', LogoOptions);
+	ScrollReveal().reveal('.right-reveal', RightOptions);
+	ScrollReveal().reveal('.left-reveal', LeftOptions);
+	ScrollReveal().reveal('.top-reveal', TopOptions);
+	ScrollReveal().reveal('.bot-reveal', BottomOptions);
+});
